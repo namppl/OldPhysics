1c0223c0e56f27ab3e79193eb88731183ba939aa
namppl <kyungwook.nam@cern.ch>
2016-01-24 17:18:44 +0100

    first commit

---
+++ NtupleMaker/src/NtupleMakerLinkDef.h
@@ -0,0 +1,29 @@
+#include "NtupleMaker.h"
+#if defined(__CLING__) || defined(__CINT__)
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ class NtupleTrigger+;
+#pragma link C++ class NtupleTriggerObject+;
+#pragma link C++ class NtupleMuon+;
+#pragma link C++ class NtupleElectron+;
+#pragma link C++ class NtupleDimuon+;
+#pragma link C++ class NtupleDielectron+;
+#pragma link C++ class NtupleEmu+;
+#pragma link C++ class NtuplePhoton+;
+#pragma link C++ class NtupleJet+;
+#pragma link C++ class NtupleMET+;
+#pragma link C++ class NtupleGenParticle+;
+#pragma link C++ class NtupleEvent+;
+#pragma link C++ class std::vector<NtupleTrigger>+;
+#pragma link C++ class std::vector<NtupleTriggerObject>+;
+#pragma link C++ class std::vector<NtupleMuon>+;
+#pragma link C++ class std::vector<NtupleElectron>+;
+#pragma link C++ class std::vector<NtupleDimuon>+;
+#pragma link C++ class std::vector<NtupleDielectron>+;
+#pragma link C++ class std::vector<NtupleEmu>+;
+#pragma link C++ class std::vector<NtuplePhoton>+;
+#pragma link C++ class std::vector<NtupleJet>+;
+#pragma link C++ class std::vector<NtupleMET>+;
+#pragma link C++ class std::vector<NtupleGenParticle>+;
+#endif
